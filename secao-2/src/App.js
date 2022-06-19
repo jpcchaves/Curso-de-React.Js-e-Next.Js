a2e8942216bfcee5d1237893b39d60773e3dd6cd
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-19 19:33:26 -0300

    secao-2

---
+++ secao-2/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {}
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map((post) => (
+          <div>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
