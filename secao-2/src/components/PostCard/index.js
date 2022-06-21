bb7368d14ed2d6013723852687745e001676c89b
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-20 22:17:16 -0300

    passando props para um component

---
+++ secao-2/src/components/PostCard/index.js
@@ -0,0 +1,15 @@
+const PostCard = (props) => {
+  const post = props.post;
+
+  return (
+    <div className="post">
+      <img src={post.cover} alt={post.title} />
+      <div className="post-content">
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
