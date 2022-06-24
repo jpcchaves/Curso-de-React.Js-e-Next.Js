4144a164b346900eb4897a031f88f9893b0793db
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-24 10:50:02 -0300

    useEffect()

--- secao-4/src/App.js
+++ secao-4/src/App.js
@@ -1,8 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  // 1- useState
   const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
   const reverseClass = reverse ? "reverse" : "";
@@ -15,9 +16,28 @@ function App() {
     setCounter(counter + 1);
   };
 
+  // 2- useEffect
+  // 2.1 - componentDidUpdate - executa toda vez que o component atualiza
+  useEffect(() => {
+    console.log("componentDidUpdate");
+  });
+
+  // 2.2 componentDidMount - executa apenas 1x
+  useEffect(() => {
+    console.log("componentDidMount");
+  }, []);
+
+  // 2.3 - executa sempre que alguma dependência for alterada
+  useEffect(() => {
+    console.log(
+      "Executa 1x ou mais quando alguma dependência for alterada. O counter mudou para:",
+      counter
+    );
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <section className="App-section">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Contador: {counter}</h1>
         <p>
@@ -30,7 +50,14 @@ function App() {
             Incrementar
           </button>
         </p>
-      </header>
+      </section>
+      <section className="App-section">
+        <h1>useEffect</h1>
+        <h3>{counter}</h3>
+        <button onClick={() => setCounter(counter + 1)}>
+          Atualizar Component
+        </button>
+      </section>
     </div>
   );
 }
