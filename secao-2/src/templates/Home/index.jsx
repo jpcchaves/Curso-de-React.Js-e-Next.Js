93dc42f251292ec88bfda880e3b2f4e94ade0b11
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-26 13:17:27 -0300

    custom hook use fetch (* IMPORTANTE *)

--- secao-2/src/templates/Home/index.jsx
+++ secao-2/src/templates/Home/index.jsx
@@ -1,179 +1,43 @@
-import "./styles.css";
-
-import { loadPosts } from "../../utils/load-posts";
-import { Posts } from "../../components/Posts";
-import { Button } from "../../components/Button";
-import { TextInput } from "../../components/TextInput";
-// Hooks
-import { useState, useEffect, useCallback } from "react";
+import { useState } from "react";
+import { useFetch } from "./use-fetch";
 
 export const Home = () => {
-  // States
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postPerPage] = useState(10);
-  const [searchValue, setSearchValue] = useState("");
+  const [postId, setPostId] = useState("");
 
-  const noMorePosts = page + postPerPage >= allPosts.length;
-
-  const filteredPosts = !!searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
-
-  // loadPosts
-  const handleLoadPosts = useCallback(async (page, postPerPage) => {
-    const postsAndPhotos = await loadPosts();
-
-    setPosts(postsAndPhotos.slice(page, postPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
-
-  // useEffect
-  useEffect(() => {
-    handleLoadPosts(0, postPerPage);
-  }, [handleLoadPosts, postPerPage]);
-
-  const loadMorePosts = () => {
-    const nextPage = page + postPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
-    posts.push(...nextPosts);
-
-    setPosts(posts);
-    setPage(nextPage);
-  };
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-  };
-
-  return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && (
-          <h1>
-            Search value:
-            <span className="search-value-span"> {searchValue}</span>
-          </h1>
-        )}
-        <TextInput searchValue={searchValue} handleChange={handleChange} />
-      </div>
-      {filteredPosts.length > 0 && (
-        <>
-          <h1>Posts:</h1>
-          <br />
-          <Posts posts={filteredPosts} />
-        </>
-      )}
-      {filteredPosts.length === 0 && (
-        <p>
-          Não existem posts com a palavra chave inserida:{" "}
-          <span className="search-value-span">{searchValue}</span>
-        </p>
-      )}
-      <div className="button-container">
-        {!searchValue && (
-          <Button
-            text="Load more posts"
-            onClick={loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
-      </div>
-    </section>
+  const [result, loading] = useFetch(
+    "https://jsonplaceholder.typicode.com/posts/" + postId,
+    {
+      headers: {
+        abc: "30" + postId,
+      },
+    }
   );
+
+  if (loading) {
+    return <p>loading....</p>;
+  }
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
+  if (!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick("")}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return <h1>oi</h1>;
 };
-
-// class Home2 extends Component {
-//   state = {
-//     posts: [],
-//     allPosts: [],
-//     page: 0,
-//     postPerPage: 2,
-//     searchValue: "",
-//   };
-
-//   async componentDidMount() {
-//     await this.loadPosts();
-//   }
-
-//   // loadPosts
-//   loadPosts = async () => {
-//     const { page, postPerPage } = this.state;
-
-//     const postsAndPhotos = await loadPosts();
-//     this.setState({
-//       posts: postsAndPhotos.slice(page, postPerPage),
-//       allPosts: postsAndPhotos,
-//     });
-//   };
-
-//   loadMorePosts = () => {
-//     const { page, postPerPage, allPosts, posts } = this.state;
-//     const nextPage = page + postPerPage;
-//     const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
-//     posts.push(...nextPosts);
-
-//     this.setState({ posts, page: nextPage });
-//   };
-
-//   handleChange = (e) => {
-//     const { value } = e.target;
-//     this.setState({ searchValue: value });
-//   };
-
-//   render() {
-//     const { posts, page, postPerPage, allPosts, searchValue } = this.state;
-//     const noMorePosts = page + postPerPage >= allPosts.length;
-
-//     const filteredPosts = !!searchValue
-//       ? allPosts.filter((post) => {
-//           return post.title.toLowerCase().includes(searchValue.toLowerCase());
-//         })
-//       : posts;
-
-//     return (
-//       <section className="container">
-//         <div className="search-container">
-//           {!!searchValue && (
-//             <h1>
-//               Search value:
-//               <span className="search-value-span"> {searchValue}</span>
-//             </h1>
-//           )}
-//           <TextInput
-//             searchValue={searchValue}
-//             handleChange={this.handleChange}
-//           />
-//         </div>
-//         {filteredPosts.length > 0 && (
-//           <>
-//             <h1>Posts:</h1>
-//             <br />
-//             <Posts posts={filteredPosts} />
-//           </>
-//         )}
-//         {filteredPosts.length === 0 && (
-//           <p>
-//             Não existem posts com a palavra chave inserida:{" "}
-//             <span className="search-value-span">{searchValue}</span>
-//           </p>
-//         )}
-//         <div className="button-container">
-//           {!searchValue && (
-//             <Button
-//               text="Load more posts"
-//               onClick={this.loadMorePosts}
-//               disabled={noMorePosts}
-//             />
-//           )}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-
-export default Home;
